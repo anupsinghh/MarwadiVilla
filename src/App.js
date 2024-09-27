9606733f5485358f2ed0284d7dc1a06672dade5d
Pratik <pratikpemendra@gmail.com>
2024-09-27 14:23:54 +0530

    adding property details page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from "./components/Footer";
 
 import Home from "./pages/Home";
 import Compare from "./pages/Compare";
+import PropertyPage from "./pages/PropertyPage";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/compare" element={<Compare />} />
+        <Route path="/property/:id" element={<PropertyPage/>}/>
         
       </Routes>
       <Footer />
